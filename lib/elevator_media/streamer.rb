5030db324677a811fdd05b926e15f7ba486600c9
Claude St-Laurent <teambiti@yahoo.com>
2021-03-30 17:06:30 -0400

    test for getweather ok

--- lib/elevator_media/streamer.rb
+++ lib/elevator_media/streamer.rb
@@ -2,18 +2,29 @@ require 'open_weather'
 module ElevatorMedia
 
     class Streamer
-        def convert(n)
-            '1'
-        end  
-        
-        def getcontent(input)
-            55
+
+        def initialize
+            @city = 'Quebec,CA'
+        end
+
+
+        def getContent(x)
+        #   @input = self.test55
+          @weather = self.getweather(@city)  
+            
         end
 
         def getweather(city)
             options = { units: "metric", APPID: "2fe833a897f6adda8d2270af6545a6ed" }
-            OpenWeather::Current.city("#{city}", options)
+            city_weather = OpenWeather::Current.city("#{city}", options)
+            puts(city_weather)
+            return city_weather
+        end
+
+        def test55
+            55
         end
     end
     
 end
+# "The weather for #{city} is #{weather}."
--- spec/elevator_media/streamer_spec.rb
+++ spec/elevator_media/streamer_spec.rb
@@ -1,29 +1,21 @@
 require 'elevator_media/streamer.rb'
 # require 'rails_helper'
 
-# describe ElevatorMedia::Streamer do                           # this try didn't work
-#     let!(:streamer){ElevatorMedia::Streamer.new}
-
-#     context 'what should I have receive?' do
-#         it 'should return "1" when receives 1' do
-#             expect(streamer.convert(1)).to '1'
-#         end
-#     end
-# end
 
 describe ElevatorMedia::Streamer do
 
     let!(:streamer){ElevatorMedia::Streamer.new}
 
-    context 'what should I have receive?' do
-        it 'should return "1" when receives 1' do
-            expect(streamer.convert(1)).to eq '1'
+    context 'Does getcontent method work' do                # try 1 doesn't work because there is no content in the method.
+        it 'yes it works' do                                # 2nd try add 55 in the method and 'yes it work' appear
+            expect(streamer).to respond_to(:getContent)
         end
     end
 
-    context 'Does getcontent method work' do                # try 1 doesn't work because there is no content in the method.
-        it 'yes it works' do                                # 2nd try add 55 in the method and 'yes it work' appear
-            expect(streamer).to respond_to(:getcontent)
+    context 'Does getcontent return 55' do                # try 1 doesn't work because there is no content in the method.
+        it 'yes it return 55' do                          # 2nd try worked with number 58 and doesn't pass
+            puts(streamer.getContent(@input))             # 3rd try add 55 in the method and 'yes it work' appear
+            expect(streamer.getContent(@input)).to eq 55
         end
     end
 
@@ -33,10 +25,12 @@ describe ElevatorMedia::Streamer do
         end
     end
 
-    context 'Does getweather by city work' do               
-        it 'return city weather' do
-            expect(streamer.getweather('Cobber Pedy')).not_to be_nil
-        end
+    context 'Does getweather by city work' do                           # try 1 doesn't work because the method wasn't write correctly
+        it 'return city weather' do                                     # 2nd still doesn't, probably the variable? 
+            expect(streamer.getContent(@weather)).not_to be_nil      # 3rd, 4th and 5th didn't work
+        end                                                             # 6th time pass, create a var with the function and return it
     end
+
+    context ''
 
 end
